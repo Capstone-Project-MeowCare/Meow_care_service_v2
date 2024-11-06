76fc3f123fff83fa183c7b555b0a5ddce258fcc7
Huynh Tran The Trong <78414286+trongtk001@users.noreply.github.com>
2024-11-06 21:38:46 +0700

    Adds endpoint to retrieve booking order with details.

--- src/main/java/com/mewcare/meow_care_service/services/Impl/BookingOrderServiceImpl.java
+++ src/main/java/com/mewcare/meow_care_service/services/Impl/BookingOrderServiceImpl.java
@@ -5,6 +5,8 @@ import com.mewcare.meow_care_service.dto.BookingOrderWithDetailDto;
 import com.mewcare.meow_care_service.dto.response.ApiResponse;
 import com.mewcare.meow_care_service.entities.BookingOrder;
 import com.mewcare.meow_care_service.entities.User;
+import com.mewcare.meow_care_service.enums.ApiStatus;
+import com.mewcare.meow_care_service.exception.ApiException;
 import com.mewcare.meow_care_service.mapper.BookingOrderMapper;
 import com.mewcare.meow_care_service.repositories.BookingOrderRepository;
 import com.mewcare.meow_care_service.services.BookingOrderService;
@@ -30,6 +32,13 @@ public class BookingOrderServiceImpl extends BaseServiceImpl<BookingOrderDto, Bo
         bookingOrder.setUser(User.builder().id(UserUtils.getCurrentUserId()).build());
         bookingOrder = repository.save(bookingOrder);
         return ApiResponse.success(mapper.toDto(bookingOrder));
+    }
+
+    @Override
+    public ApiResponse<BookingOrderWithDetailDto> getWithDetail(UUID id) {
+        BookingOrder bookingOrder = repository.findById(id)
+                .orElseThrow(() -> new ApiException(ApiStatus.NOT_FOUND));
+        return ApiResponse.success(mapper.toDtoWithDetail(bookingOrder));
     }
 
     @Override
