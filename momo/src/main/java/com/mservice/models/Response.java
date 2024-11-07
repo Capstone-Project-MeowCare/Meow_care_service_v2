2998b09fc22bd2c02aa1ce5a57feb00ffd0912df
Huynh Tran The Trong <78414286+trongtk001@users.noreply.github.com>
2024-11-07 21:36:23 +0700

    Adds Momo module to the project.

---
+++ momo/src/main/java/com/mservice/models/Response.java
@@ -0,0 +1,52 @@
+package com.mservice.models;
+
+public class Response {
+    protected long responseTime;
+    protected String message;
+    protected Integer resultCode;
+    private String partnerCode;
+    private String orderId;
+    public Response() {
+        this.responseTime = System.currentTimeMillis();
+    }
+
+    public long getResponseTime() {
+        return System.currentTimeMillis();
+    }
+
+    public void setResponseTime(long responseTime) {
+        this.responseTime = responseTime;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPartnerCode() {
+        return partnerCode;
+    }
+
+    public void setPartnerCode(String partnerCode) {
+        this.partnerCode = partnerCode;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(Integer resultCode) {
+        this.resultCode = resultCode;
+    }
+}
