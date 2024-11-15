1b7548e58e2b023440a51e7a11c3f8f3eb2c3120
Huynh Tran The Trong <78414286+trongtk001@users.noreply.github.com>
2024-11-16 05:27:50 +0700

    AppSaveConfigServiceImpl

--- src/main/java/com/meow_care/meow_care_service/services/Impl/AppSaveConfigServiceImpl.java
+++ src/main/java/com/meow_care/meow_care_service/services/Impl/AppSaveConfigServiceImpl.java
@@ -7,7 +7,9 @@ import com.meow_care.meow_care_service.mapper.AppSaveConfigMapper;
 import com.meow_care.meow_care_service.repositories.AppSaveConfigRepository;
 import com.meow_care.meow_care_service.services.AppSaveConfigService;
 import com.meow_care.meow_care_service.services.base.BaseServiceImpl;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AppSaveConfigServiceImpl extends BaseServiceImpl<AppSaveConfigDto, AppSaveConfig, AppSaveConfigRepository, AppSaveConfigMapper> implements AppSaveConfigService {
     public AppSaveConfigServiceImpl(AppSaveConfigRepository repository, AppSaveConfigMapper mapper) {
         super(repository, mapper);
