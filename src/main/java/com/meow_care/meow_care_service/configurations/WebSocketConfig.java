faa79fc273a080461a316692819f159e08eb97fb
Huynh Tran The Trong <78414286+trongtk001@users.noreply.github.com>
2024-11-16 05:31:15 +0700

    WebSocketConfig

---
+++ src/main/java/com/meow_care/meow_care_service/configurations/WebSocketConfig.java
@@ -0,0 +1,15 @@
+package com.meow_care.meow_care_service.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+
+    }
+}
