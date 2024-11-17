3e93488223a24a4a7fb45347547f96d1319f24d2
Huynh Tran The Trong <78414286+trongtk001@users.noreply.github.com>
2024-11-17 12:21:57 +0700

    Removed unused test and WebSocket configurations.

---
+++ src/test/java/com/meow_care/meow_care_service/services/NotificationServiceTest.java
@@ -0,0 +1,20 @@
+package com.meow_care.meow_care_service.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NotificationServiceTest {
+
+    @BeforeEach
+    void setUp() {
+        // Setup code if needed
+    }
+
+    @Test
+    void saveNotificationSuccessfully() {
+    }
+
+
+}
