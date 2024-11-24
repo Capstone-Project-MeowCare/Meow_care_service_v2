cd6a976fa877e86509e362beced0cba504543e6b
Huynh Tran The Trong <78414286+trongtk001@users.noreply.github.com>
2024-11-25 00:43:49 +0700

    Removes unnecessary status field annotations.

--- src/main/java/com/meow_care/meow_care_service/entities/SitterFormRegister.java
+++ src/main/java/com/meow_care/meow_care_service/entities/SitterFormRegister.java
@@ -1,10 +1,7 @@
 package com.meow_care.meow_care_service.entities;
 
 import com.meow_care.meow_care_service.enums.SitterFormRegisterStatus;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -40,8 +37,6 @@ public class SitterFormRegister {
 
     private String address;
 
-    @Enumerated(EnumType.STRING)
-    @Column(length = 50)
     private SitterFormRegisterStatus status;
 
 }
