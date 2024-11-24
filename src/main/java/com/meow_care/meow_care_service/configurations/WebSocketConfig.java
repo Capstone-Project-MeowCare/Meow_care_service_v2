cd1c5b46bc7e9cb0e6b3acf748a7b63a14bfa8b6
Huynh Tran The Trong <78414286+trongtk001@users.noreply.github.com>
2024-11-24 17:12:41 +0700

    Removed Socket.IO; switched to Spring WebSocket.

--- src/main/java/com/meow_care/meow_care_service/configurations/WebSocketConfig.java
+++ src/main/java/com/meow_care/meow_care_service/configurations/WebSocketConfig.java
@@ -1,22 +1,24 @@
 package com.meow_care.meow_care_service.configurations;
 
-import com.corundumstudio.socketio.SocketIOServer;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-public class WebSocketConfig {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Value("${socket.port}")
-    private int port;
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
 
-    @Bean
-    public SocketIOServer socketIOServer() {
-        com.corundumstudio.socketio.Configuration config =
-                new com.corundumstudio.socketio.Configuration();
-        config.setPort(port);
-        return new SocketIOServer(config);
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
 }
--- src/main/java/com/meow_care/meow_care_service/controller/MyWebSocketHandler.java
+++
@@ -1,7 +0,0 @@
-package com.meow_care.meow_care_service.controller;
-
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-public class MyWebSocketHandler  extends TextWebSocketHandler {
-
-}
