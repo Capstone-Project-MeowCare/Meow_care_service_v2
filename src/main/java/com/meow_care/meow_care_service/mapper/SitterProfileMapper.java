14f3b36ec49b2411fe6d93845d0dabb54872acf9
Huynh Tran The Trong <78414286+trongtk001@users.noreply.github.com>
2024-11-24 20:35:33 +0700

    Adds profile picture linking in SitterProfileMapper.

--- src/main/java/com/meow_care/meow_care_service/mapper/SitterProfileMapper.java
+++ src/main/java/com/meow_care/meow_care_service/mapper/SitterProfileMapper.java
@@ -3,9 +3,11 @@ package com.meow_care.meow_care_service.mapper;
 import com.meow_care.meow_care_service.dto.SitterProfileDto;
 import com.meow_care.meow_care_service.dto.SitterProfileWithUserDto;
 import com.meow_care.meow_care_service.entities.SitterProfile;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING, uses = {UserMapper.class, ProfilePictureMapper.class})
@@ -20,5 +22,10 @@ public interface SitterProfileMapper extends BaseMapper<SitterProfileDto, Sitter
     SitterProfile toEntity(SitterProfileWithUserDto sitterProfileWithUserDto);
 
     SitterProfileWithUserDto toDtoWithUser(SitterProfile sitterProfile);
+
+    @AfterMapping
+    default void linkProfilePictures(@MappingTarget SitterProfile sitterProfile) {
+        sitterProfile.getProfilePictures().forEach(profilePicture -> profilePicture.setSitterProfile(sitterProfile));
+    }
 
 }
