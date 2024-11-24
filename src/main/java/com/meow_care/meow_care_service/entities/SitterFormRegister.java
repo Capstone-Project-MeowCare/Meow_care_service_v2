60c60a5fe74c5d0575227a33d5161479728faa7f
Huynh Tran The Trong <78414286+trongtk001@users.noreply.github.com>
2024-11-25 00:34:39 +0700

    Adds status field to SitterFormRegister entity.

--- src/main/java/com/meow_care/meow_care_service/entities/SitterFormRegister.java
+++ src/main/java/com/meow_care/meow_care_service/entities/SitterFormRegister.java
@@ -1,7 +1,10 @@
 package com.meow_care.meow_care_service.entities;
 
 import com.meow_care.meow_care_service.enums.SitterFormRegisterStatus;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -37,6 +40,8 @@ public class SitterFormRegister {
 
     private String address;
 
+    @Enumerated(EnumType.STRING)
+    @Column(length = 50)
     private SitterFormRegisterStatus status;
 
 }
