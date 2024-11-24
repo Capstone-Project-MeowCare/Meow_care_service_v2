ad8922c9f631bbb7227bff5b75c2e71e73dcbf70
Huynh Tran The Trong <78414286+trongtk001@users.noreply.github.com>
2024-11-24 10:36:49 +0700

    Adds SocketIO support and configures port 8000.

---
+++ src/main/java/com/meow_care/meow_care_service/configurations/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.meow_care.meow_care_service.configurations;
+
+import com.corundumstudio.socketio.SocketIOServer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSocketConfig {
+
+    @Value("${socket.port}")
+    private int port;
+
+    @Bean
+    public SocketIOServer socketIOServer() throws Exception {
+        com.corundumstudio.socketio.Configuration config =
+                new com.corundumstudio.socketio.Configuration();
+        config.setPort(port);
+        return new SocketIOServer(config);
+    }
+
+}
